ebf823b33869f09a4985ab3d0b565673716498e2
Darya Zakhar <daryazakhar.89@gmail.com>
2023-05-03 23:16:55 +0400

    feat: create user schema

---
+++ src/db/user.js
@@ -0,0 +1,48 @@
+const USERS = [
+  {
+    id: "b960c3b6-4653-415a-b9d8-8aa23d9fa35b",
+    userName: "John",
+    age: "10",
+    hobbies: "footbal",
+  },
+  {
+    id: "109156be-c4fb-41ea-b1b4-efe1671c5836",
+    userName: "Alex",
+    age: "15",
+    hobbies: "footbal",
+  },
+];
+
+const getUsers = async () => USERS;
+
+const getUserById = async (userId) => USERS.find((user) => user.id === userId);
+
+const addUser = async (user) => {
+  USERS.push(user);
+
+  return user;
+};
+
+const updateUser = async (userId, data) => {
+  let userToUpdate = await getUserById(userId);
+  
+  return Object.assign(userToUpdate, data);
+};
+
+const deleteUser = async (userId) => {
+  let deletedUser;
+  const userIndexToDelete = USERS.findIndex((user) => user.id === userId);
+
+  if (userIndexToDelete > -1) {
+    deletedUser = USERS.splice(userIndexToDelete, 1)[0];
+  }
+  return deletedUser;
+};
+
+export default {
+  getUsers,
+  getUserById,
+  addUser,
+  updateUser,
+  deleteUser,
+};
