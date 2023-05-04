42e41e05b456a32b374b4a2910aebf63b92ca1f2
Darya Zakhar <daryazakhar.89@gmail.com>
2023-05-04 16:51:57 +0400

    refactor: update package.json and add  .eslinttrc.js file

---
+++ .eslinttrc.js
@@ -0,0 +1,15 @@
+export default {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: "airbnb-base",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    indent: ["error", 2],
+  },
+};
