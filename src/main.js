a15fef416e04e93ee70eb103982a77a91188bf17
Darya Zakhar <daryazakhar.89@gmail.com>
2023-05-06 11:47:25 +0400

    refactor: add error handler for 500 status code

--- src/main.js
+++ src/main.js
@@ -5,8 +5,26 @@ import userRouter from "./routes/UserRouter.js";
 
 const { PORT } = config;
 
-const server = createServer(userRouter.handleRequest.bind(userRouter));
+const serverErrorHandler = (res) => {
+  res.statusCode = 500;
+  res.write("Something was wrong on server side");
+  res.end();
+};
 
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+export default async () => {
+  const responseHandler = async (req, res) => {
+    try {
+      await userRouter.handleRequest(req, res);
+    } catch (err) {
+      serverErrorHandler(res);
+    }
+  };
+  const server = createServer(responseHandler);
+
+  return await new Promise((res, rej) => {
+    server.listen(PORT, async () => {
+      console.log(`Server is listening on port ${PORT}`);
+      res(server);
+    });
+  })
+};
