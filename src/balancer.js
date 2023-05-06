b22eac3e42f74250658af822691f0bf06421c60a
Darya Zakhar <daryazakhar.89@gmail.com>
2023-05-06 22:54:50 +0400

    refactor: implement horisontal scaling with claster API

--- src/balancer.js
+++ src/balancer.js
@@ -1,19 +1,84 @@
 import cluster from "cluster";
-import { createServer } from "http";
 import os from "os";
+import { createServer } from "http";
+import http from "http";
 import process from "process";
-
+import startServer from "./main.js";
 import { config } from "./config.js";
 
 const { PORT } = config;
-
 const numCPUs = os.cpus().length;
 
-if (cluster.isPrimary) {
-  console.log(`Master process with PID ${process.pid} is running`);
+function workerPort() {
+  let currentPort = +PORT;
+  const port = +PORT;
+  return function () {
+    currentPort += 1;
 
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
+    if (currentPort > port + numCPUs) {
+      currentPort = port + 1;
+    }
+
+    return currentPort;
+  };
+}
+
+const getWorkerPort = workerPort();
+
+function isDataUpdated(masterData, workerData) {
+  return JSON.stringify(masterData) !== JSON.stringify(workerData);
+}
+
+function sendMessageToWorkers(workers, message) {
+  for (const worker of workers) {
+    worker.send(message);
+  }
+}
+
+if (cluster.isPrimary) {
+  function onRequest(client_req, client_res) {
+    const options = {
+      path: client_req.url,
+      method: client_req.method,
+      headers: client_req.headers,
+    };
+
+    const proxy = http.request(
+      "http://localhost:" + getWorkerPort(),
+      options,
+      function (res) {
+        client_res.writeHead(res.statusCode, res.headers);
+        res.pipe(client_res, {
+          end: true,
+        });
+      }
+    );
+
+    client_req.pipe(proxy, {
+      end: true,
+    });
+
+    client_res.once("finish", () => {
+      sendMessageToWorkers(Object.values(cluster.workers), {
+        message: "getDataFromWorker",
+        currentUsers: process.USERS,
+      });
+    });
+  }
+
+  createServer(onRequest).listen(PORT, () => {
+    console.log(`Main server is listening on port ${PORT}`);
+  });
+
+  for (let i = 1; i <= numCPUs; i++) {
+    const worker = cluster.fork({ PORT: +PORT + i });
+    worker.on("message", (users) => {
+      sendMessageToWorkers(Object.values(cluster.workers), {
+        message: "setDataToWorker",
+        data: users,
+      });
+      process.USERS = users;
+    });
   }
 
   cluster.on("exit", (worker, code, signal) => {
@@ -22,14 +87,15 @@ if (cluster.isPrimary) {
     );
   });
 } else {
-  const server = createServer((req, res) => {
-    res.writeHead(200);
-    res.end(`Worker ${process.pid} handled request`);
-  });
-
-  server.listen(+PORT + +cluster.worker.id, () => {
-    console.log(
-      `Worker ${process.pid} is listening on port ${+PORT + +cluster.worker.id}`
-    );
+  await startServer();
+  process.on("message", (data) => {
+    if (
+      data.message === "getDataFromWorker" &&
+      isDataUpdated(process.USERS, data.currentUsers)
+    ) {
+      process.send(process.USERS);
+    } else if (data.message === "setDataToWorker") {
+      process.USERS = data.data;
+    }
   });
 }
